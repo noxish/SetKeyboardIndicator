5b653f52534ff3ab2c45d7cf17901f8785feb580
Stefano Hämmerle <NoxisH@hotmail.com>
2019-04-29 13:37:38 +0200

    Projektdateien hinzufügen.

---
+++ SetKeyboardIndicator/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim readValue = My.Computer.Registry.GetValue(
+    "HKEY_USERS\.DEFAULT\Control Panel\Keyboard", "InitialKeyboardIndicators", Nothing)
+        Dim myInteger As Double
+        myInteger = Convert.ToDouble(readValue.ToString)
+
+        My.Computer.Registry.SetValue("HKEY_USERS\.DEFAULT\Control Panel\Keyboard", "InitialKeyboardIndicators", myInteger + 2)
+
+    End Sub
+
+End Module
