98db18aaeb4b21dcf9b2b5c06098d3061dc234f7
Stefano Hämmerle <NoxisH@hotmail.com>
2019-04-30 14:13:59 +0200

    Added Comments

--- SetKeyboardIndicator/Module1.vb
+++ SetKeyboardIndicator/Module1.vb
@@ -1,11 +1,18 @@
 ﻿Module Module1
 
     Sub Main()
+
+
+        'Get Value from Registry'
         Dim readValue = My.Computer.Registry.GetValue(
     "HKEY_USERS\.DEFAULT\Control Panel\Keyboard", "InitialKeyboardIndicators", Nothing)
         Dim myInteger As Double
+
+        'Convert Object to Double'
         myInteger = Convert.ToDouble(readValue.ToString)
 
+
+        'Write Value into Registry'
         My.Computer.Registry.SetValue("HKEY_USERS\.DEFAULT\Control Panel\Keyboard", "InitialKeyboardIndicators", myInteger + 2)
 
     End Sub
